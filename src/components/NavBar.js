399451cf3a7cd44799f4612bde35ea14fa6b87c6
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-21 12:53:36 -0400

    imported router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import '../App.css'
 import { Row, Col, PageHeader, Input, Menu, Icon } from 'antd'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 
 const { Search } = Input;
 
 export default class NavBar extends Component {
-    state = {
-        search : false
-    }
-
-    toggleSearch = () => {
-        let search = !this.state.search
-        this.setState({search})
-    }
     render() {
         return (
             <header>
@@ -22,13 +16,12 @@ export default class NavBar extends Component {
                     <Col span={8}>
                         <PageHeader title='Youtube Search' />
                     </Col>
-                   {this.state.search ? <Col span={9} style={{marginTop: '17px', marginRight: '20px'}}>
+                   <Col span={9} style={{marginTop: '17px', marginRight: '20px'}}>
                         <Search placeholder="input search text" enterButton />
-                    </Col> : null
-                   }
+                    </Col>
                     <Col span={6}>
                 <Menu mode="horizontal" style={{ float: 'right', color: 'red'}}>
-                    <Menu.Item key='search' onClick={this.toggleSearch}><Icon type="search" />Search</Menu.Item>
+                    <Menu.Item key='home'><Icon type="home" />Home</Menu.Item>
                     <Menu.Item key='liked'><Icon type="like" />Liked</Menu.Item>
                     <Menu.Item key='account'><Icon type="setting" />Account</Menu.Item>
                 </Menu>
