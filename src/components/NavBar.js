176260fbffcb9d0e89fc18e97c8a80a16f089d57
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-21 12:42:19 -0400

    toggle search

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,14 @@ import { Row, Col, PageHeader, Input, Menu, Icon } from 'antd'
 const { Search } = Input;
 
 export default class NavBar extends Component {
+    state = {
+        search : false
+    }
+
+    toggleSearch = () => {
+        let search = !this.state.search
+        this.setState({search})
+    }
     render() {
         return (
             <header>
@@ -14,12 +22,13 @@ export default class NavBar extends Component {
                     <Col span={8}>
                         <PageHeader title='Youtube Search' />
                     </Col>
-                    <Col span={9} style={{marginTop: '17px', marginRight: '20px'}}>
+                   {this.state.search ? <Col span={9} style={{marginTop: '17px', marginRight: '20px'}}>
                         <Search placeholder="input search text" enterButton />
-                    </Col>
+                    </Col> : null
+                   }
                     <Col span={6}>
                 <Menu mode="horizontal" style={{ float: 'right', color: 'red'}}>
-                    <Menu.Item key='search'><Icon type="search" />Search</Menu.Item>
+                    <Menu.Item key='search' onClick={this.toggleSearch}><Icon type="search" />Search</Menu.Item>
                     <Menu.Item key='liked'><Icon type="like" />Liked</Menu.Item>
                     <Menu.Item key='account'><Icon type="setting" />Account</Menu.Item>
                 </Menu>
