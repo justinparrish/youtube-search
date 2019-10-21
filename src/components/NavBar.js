ee7763505c6e51e9926482a2858be2ebcf00ac68
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-21 12:35:07 -0400

    positioning search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css';
 import '../App.css'
-import { Row, Col, PageHeader } from 'antd'
-import { Menu, Icon } from 'antd';
+import { Row, Col, PageHeader, Input, Menu, Icon } from 'antd'
 
 
-const { SubMenu } = Menu;
+const { Search } = Input;
 
 export default class NavBar extends Component {
     render() {
         return (
             <header>
-                <Row gutter={[8,16]}>
+                <Row gutter={[16,16]}>
                     <Col span={8}>
-                <PageHeader title='Youtube Search' />
+                        <PageHeader title='Youtube Search' />
                     </Col>
-                    <Col span={16}>
-                <Menu mode="horizontal" style={{ textAlign: 'right', color: 'red'}}>
+                    <Col span={9} style={{marginTop: '17px', marginRight: '20px'}}>
+                        <Search placeholder="input search text" enterButton />
+                    </Col>
+                    <Col span={6}>
+                <Menu mode="horizontal" style={{ float: 'right', color: 'red'}}>
                     <Menu.Item key='search'><Icon type="search" />Search</Menu.Item>
                     <Menu.Item key='liked'><Icon type="like" />Liked</Menu.Item>
                     <Menu.Item key='account'><Icon type="setting" />Account</Menu.Item>
