3deb41289533c7d915ec6c0eb0caad4332d46e58
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-21 12:18:59 -0400

    added search form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css';
-import { Row, Col } from 'antd'
+import '../App.css'
+import { Row, Col, PageHeader } from 'antd'
 import { Menu, Icon } from 'antd';
-import MenuItem from 'antd/lib/menu/MenuItem';
+
 
 const { SubMenu } = Menu;
 
-export class NavBar extends Component {
+export default class NavBar extends Component {
     render() {
         return (
             <header>
-            <Menu mode="horizontal"style={{marginLeft: '20em'}}>
-                <Menu.Item disabled key='title'>Youtube Search</Menu.Item>
-                <Menu.Item key='search'><Icon type="search" />Search</Menu.Item>
-                <Menu.Item key='liked'><Icon type="like" />Liked</Menu.Item>
-                <Menu.Item key='account'><Icon type="setting" />Account</Menu.Item>
-            </Menu>
+                <Row gutter={[8,16]}>
+                    <Col span={8}>
+                <PageHeader title='Youtube Search' />
+                    </Col>
+                    <Col span={16}>
+                <Menu mode="horizontal" style={{ textAlign: 'right', color: 'red'}}>
+                    <Menu.Item key='search'><Icon type="search" />Search</Menu.Item>
+                    <Menu.Item key='liked'><Icon type="like" />Liked</Menu.Item>
+                    <Menu.Item key='account'><Icon type="setting" />Account</Menu.Item>
+                </Menu>
+                    </Col>
+
+                </Row>
             </header>
         )
     }
 }
-
-export default NavBar
